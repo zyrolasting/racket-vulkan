c0ddf494cdc243b360c82193626fa3a6238ceb0a
Sage Gerard <sage@sagegerard.com>
2019-10-18 13:06:50 -0400

    Add txexpr dependency

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "vulkan")
-(define deps '("base"))
+(define deps '("base" "txexpr"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/vulkan.scrbl" ())))
 (define pkg-desc "Vulkan integration for Racket")
