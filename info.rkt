72b92c65dc8990539261be338e3119fe9562344d
Sage Gerard <sage@sagegerard.com>
2019-10-19 18:15:24 -0400

    Depend on graph-lib

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "vulkan")
-(define deps '("base" "txexpr"))
+(define deps '("base" "txexpr" "graph-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/vulkan.scrbl" ())))
 (define pkg-desc "Vulkan integration for Racket")
