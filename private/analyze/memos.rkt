4e357f529c467b10aad6c9d2ac2cdd51bb780bb4
Sage Gerard <sage@sagegerard.com>
2019-11-05 16:46:48 -0500

    Use parameter for memo cache

--- private/analyze/memos.rkt
+++ private/analyze/memos.rkt
@@ -1,15 +1,27 @@
 #lang racket/base
 
+;; -------------------------------------------------------------
+;; Responsible for caching the many expensive operations used to
+;; gather information from the Vulkan spec.
+
+(require racket/contract
+         racket/dict)
+
 (provide (all-defined-out))
+
 (module+ test
   (require rackunit))
 
-(define (memoizer proc)
-  (define val #f)
+(define/contract current-memo-cache
+  (parameter/c dict?)
+  (make-parameter (make-hash)))
+
+(define (memoizer proc [key (gensym)])
   (λ A
-    (unless val
-      (set! val (apply proc A)))
-    val))
+    (define cache (current-memo-cache))
+    (unless (hash-has-key? cache key)
+      (hash-set! cache key (apply proc A)))
+    (hash-ref cache key)))
 
 (module+ test
   (test-case "Save return value from call"
@@ -20,4 +32,8 @@
     (check-eq? (a) (a))
     (check-eqv? a-calls 1)
     (check-eq? (a 1) (a 1))
-    (check-eqv? a-calls 1)))
+    (check-eqv? a-calls 1)
+    (test-case "New cache, new calls"
+      (parameterize ([current-memo-cache (make-hash)])
+        (check-eq? (a 1) (a 1))
+        (check-eqv? a-calls 2)))))
