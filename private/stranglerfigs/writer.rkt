4e7409ebe76f9fcf8efec747ffb4e041177cc9c1
Sage Gerard <sage@sagegerard.com>
2019-11-05 13:18:21 -0500

    Use correct port and add comments

--- private/stranglerfigs/writer.rkt
+++ private/stranglerfigs/writer.rkt
@@ -3,13 +3,23 @@
 (provide (all-defined-out))
 (require "./paths.rkt")
 
+; This is how we deliver Racket code generated using the Vulkan spec.
+; This procedure is kept seperate for the sake of a unit test.
 (define (write-sequence registry make-sequence [out (current-output-port)])
   (for ([declaration (make-sequence registry)])
     (writeln declaration out)))
 
-(define (write-public-module! path-element registry make-sequence)
+
+(define (write-racket-module-file! path registry make-sequence)
   (call-with-output-file #:exists 'replace
-    (build-path package-path path-element)
+    path
     (λ (port)
-      (displayln "#lang racket/base")
-      (write-sequence registry make-sequence))))
+      (displayln "#lang racket/base" port) ; Limits load time
+      (write-sequence registry make-sequence port))))
+
+
+; For maintainer use only.
+(define (write-package-module-file! registry make-sequence . path-elements)
+  (write-racket-module-file! (apply build-path package-path path-elements)
+                             registry
+                             make-sequence))
