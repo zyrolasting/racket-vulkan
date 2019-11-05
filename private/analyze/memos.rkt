1373ddd5facb8a1326fea59640eb3280eed955e9
Sage Gerard <sage@sagegerard.com>
2019-11-05 14:30:46 -0500

    Revert memoizer implementation
    Comparing the Vulkan spec's xexpr value against keys in the hash
    turned out to be too expensive, and `memoizer`'s upgrade did
    not have any application yet. Roll back the implementation,
    but keep the name.

--- private/analyze/memos.rkt
+++ private/analyze/memos.rkt
@@ -5,16 +5,14 @@
   (require rackunit))
 
 (define (memoizer proc)
-  (define memos (make-hash))
+  (define val #f)
   (λ A
-    (if (hash-has-key? memos A)
-        (hash-ref memos A)
-        (let ([result (apply proc A)])
-          (hash-set! memos A result)
-          result))))
+    (unless val
+      (set! val (apply proc A)))
+    val))
 
 (module+ test
-  (test-case "Can memoize based on equal?-comparable argument lists"
+  (test-case "Save return value from call"
     (define a-calls 0)
     (define a (memoizer (λ _
                           (set! a-calls (add1 a-calls))
@@ -22,4 +20,4 @@
     (check-eq? (a) (a))
     (check-eqv? a-calls 1)
     (check-eq? (a 1) (a 1))
-    (check-eqv? a-calls 2)))
+    (check-eqv? a-calls 1)))
