adcf11a3bccd6a9e14cc20155704c3f25830f23b
Sage Gerard <sage@sagegerard.com>
2019-11-06 13:02:12 -0500

    Remove needless 'symdecl' category

--- private/generate/make-unsafe.rkt
+++ private/generate/make-unsafe.rkt
@@ -54,8 +54,7 @@
 
 (module+ test
   (test-equal? "(generate-define-signature)"
-               (generate-define-signature '(type ((category "symdecl")
-                                                  (name "VkDeviceAddress"))))
+               (generate-define-signature '(type ((name "VkDeviceAddress"))))
                '(define _VkDeviceAddress 'VkDeviceAddress)))
 
 ;; ------------------------------------------------
